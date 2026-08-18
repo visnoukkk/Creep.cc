66a8099dfdbcdea141fef47abff5d186596b3941
visnoukkk <chamnanchanvathana@gmail.com>
2026-08-18 11:00:53 +0700

    Add new themes and update existing theme data

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -13,7 +13,9 @@ local ThemeManager = {} do
 		['Tokyo Night'] 	= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
 		['Ubuntu'] 			= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
 		['Quartz'] 			= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232330","AccentColor":"426e87","BackgroundColor":"1d1b26","OutlineColor":"27232f"}') },
-['Creep'] = { 9, httpService:JSONDecode('{"FontColor":"c5c3c3","MainColor":"000000","AccentColor":"ffffff","BackgroundColor":"000000","OutlineColor":"27232f"}') },
+		['tokoyo.wtf'] 		= { 9, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"181818","AccentColor":"4777b6","BackgroundColor":"141414","OutlineColor":"141414"}') },
+		['Unnamed Enhancements'] 		= { 10, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"181818","AccentColor":"ffffff","BackgroundColor":"141414","OutlineColor":"141414"}') },
+['Creep'] = { 11, httpService:JSONDecode('{"FontColor":"c5c3c3","MainColor":"000000","AccentColor":"ffffff","BackgroundColor":"000000","OutlineColor":"27232f"}') },
 	}
 
 	function ThemeManager:ApplyTheme(theme)
